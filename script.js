aff9a445e1bdd06d6a0f00963c36459fca7ac088
Chadd <chadd_artaud@cox.net>
2022-03-28 10:29:51 -0700

    gave cpu multiple response options on win

--- script.js
+++ script.js
@@ -86,55 +86,13 @@ function displayScore(cpuScore, playerScore){
     }
     else if (cpuScore == 5) {
         winner.style.color = "red";
-        winner.textContent = "Looks like the machine is superior after all. Better luck next time!";
+        winner.textContent = "formulating response.... ";
+        const pause = setTimeout(cpuResponse, 5000);
+        function cpuResponse() {
+            let rand = Math.floor((Math.random()*3));
+            let arr = ['Better luck next time loser!', 'Wow you stink!', 'Hacking into savings account'];
+            winner.textContent = `${arr[rand]}`;
+        }
     }
 }
-
-
-
-
-
-/*Write a NEW function called game(). Call the playRound function inside of this one to play a 5 
-round game that keeps score and reports a winner or loser at the end.*/
-
-/*
-function game(){
-    let cpuScore = 0;
-    let playerScore = 0;     
-
-    for (let round = 1; round <= 5; round++){
-        let turn = playRound(playerPlay(), computerPlay());
-        if (turn[4] == 'w'){
-            playerScore++; 
-            if (playerScore == 3) {
-                displayResult(`Darn you win! The final score is player: ${playerScore} computer: ${cpuScore}`);
-                break
-            }
-            else {
-                 displayResult(`${turn} The score of round ${round} is player: ${playerScore} computer: ${cpuScore}`);
-            }
-        }
-        else if (turn[4] == 'l') {
-            cpuScore++;
-            if (cpuScore == 3) {
-                displayResult(`Looks like the machine is superior this time! The final score was: player: ${playerScore} computer: ${cpuScore}`)
-                break
-            }
-            else {
-                displayResult(`${turn} The score of round ${round} is player: ${playerScore} computer: ${cpuScore}`);
-            }
-        }
-        else if (turn[0] == 'I') {
-            round--;
-            console.log(turn + '\nThis round doesn\'t count.');
-        }
-        else {
-            round--;
-            console.log('This round doesn\'t count. Please make sure you enter rock, paper, or scissors');
-        }
-        
-    }
-}
-*/
-
 
